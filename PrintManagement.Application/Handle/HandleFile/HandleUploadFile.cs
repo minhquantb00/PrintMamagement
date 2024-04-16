5908d3ed69a6bf9e734b4537374fb372775333fa
minhquantb00 <minhquantb00@gmail.com>
2024-04-16 09:27:21 +0700

    init

--- PrintManagement.Application/Handle/HandleFile/HandleUploadFile.cs
+++ PrintManagement.Application/Handle/HandleFile/HandleUploadFile.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.StaticFiles;
 using System;
 using System.Collections.Generic;
@@ -36,6 +38,36 @@ namespace PrintManagement.Application.Handle.HandleFile
             return fileName;
         }
 
+
+
+        static string cloudName = "dacc055vz";
+        static string apiKey = "359551439884411";
+        static string apiSecret = "8MKLtn4MyEl3w6STTwZwGiCuuGM";
+        static public Account account = new Account(cloudName, apiKey, apiSecret);
+        static public Cloudinary _cloudinary = new Cloudinary(account);
+        public static async Task<string> Upfile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentException("Không có tập tin được chọn.");
+            }
+            using (var stream = file.OpenReadStream())
+            {
+                var uploadParams = new ImageUploadParams()
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    PublicId = "xyz-abc" + "_" + DateTime.Now.Ticks + "image",
+                    Transformation = new Transformation().Width(400).Height(400).Crop("fit")
+                };
+                var uploadResult = await HandleUploadFile._cloudinary.UploadAsync(uploadParams);
+                if (uploadResult.Error != null)
+                {
+                    throw new Exception(uploadResult.Error.Message);
+                }
+                string imageUrl = uploadResult.SecureUrl.ToString();
+                return imageUrl;
+            }
+        }
 
     }
 }

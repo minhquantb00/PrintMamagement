5cb95090eca3a7b0ad71974f4ef72783e8a8cde0
DuongTanjiro <duongcutewa@gmail.com>
2024-04-22 10:13:46 +0700

    init

--- PrintManagement.FrontEnd/full-version/src/navigation/horizontal/apps.js
+++ PrintManagement.FrontEnd/full-version/src/navigation/horizontal/apps.js
@@ -1,49 +1,55 @@
 export default [
   {
-    title: 'Apps',
-    icon: { icon: 'tabler-layout-grid-add' },
+    title: "Apps",
+    icon: { icon: "tabler-layout-grid-add" },
     children: [
       {
-        title: 'Email',
-        icon: { icon: 'tabler-mail' },
-        to: 'apps-email',
+        title: "Email",
+        icon: { icon: "tabler-mail" },
+        to: "apps-email",
       },
       {
-        title: 'Chat',
-        icon: { icon: 'tabler-message-circle' },
-        to: 'apps-chat',
+        title: "Chat",
+        icon: { icon: "tabler-message-circle" },
+        to: "apps-chat",
       },
       {
-        title: 'Calendar',
-        to: 'apps-calendar',
-        icon: { icon: 'tabler-calendar' },
+        title: "Calendar",
+        to: "apps-calendar",
+        icon: { icon: "tabler-calendar" },
       },
       {
-        title: 'Invoice',
-        icon: { icon: 'tabler-file-dollar' },
+        title: "Invoice",
+        icon: { icon: "tabler-file-dollar" },
         children: [
-          { title: 'List', to: 'apps-invoice-list' },
-          { title: 'Preview', to: { name: 'apps-invoice-preview-id', params: { id: '5036' } } },
-          { title: 'Edit', to: { name: 'apps-invoice-edit-id', params: { id: '5036' } } },
-          { title: 'Add', to: 'apps-invoice-add' },
+          { title: "List", to: "apps-invoice-list" },
+          {
+            title: "Preview",
+            to: { name: "apps-invoice-preview-id", params: { id: "5036" } },
+          },
+          {
+            title: "Edit",
+            to: { name: "apps-invoice-edit-id", params: { id: "5036" } },
+          },
+          { title: "Add", to: "apps-invoice-add" },
         ],
       },
       {
-        title: 'User',
-        icon: { icon: 'tabler-users' },
+        title: "User",
+        icon: { icon: "tabler-users" },
         children: [
-          { title: 'List', to: 'apps-user-list' },
-          { title: 'View', to: { name: 'apps-user-view' },
+          { title: "List", to: "apps-user-list" },
+          { title: "View", to: { name: "apps-user-view" } },
         ],
       },
       {
-        title: 'Roles & Permissions',
-        icon: { icon: 'tabler-settings' },
+        title: "Roles & Permissions",
+        icon: { icon: "tabler-settings" },
         children: [
-          { title: 'Roles', to: 'apps-roles' },
-          { title: 'Permissions', to: 'apps-permissions' },
+          { title: "Roles", to: "apps-roles" },
+          { title: "Permissions", to: "apps-permissions" },
         ],
       },
     ],
   },
-]
+];
